d19beed9a509ff2340bdf87d666acbc27fc45bad
8an <8an.akrish@gmail.com>
2022-04-28 14:59:15 +0300

    store

--- store.js
+++ store.js
@@ -2,29 +2,60 @@ const itemsUl = document.getElementById("items");
 const cartUl = document.getElementById("cart");
 const price = document.getElementById("payment");
 const breakdown = document.getElementById("payment-breakdown");
+const clear = document.getElementById("clear");
+itemsCart = document.getElementById("itemsCart");
+const store = localStorage.getItem("store");
+let items = [];
+clear.onclick = () => {
+  localStorage.removeItem("store");
+  console.log("cleared");
+  fillItems();
+  rerender();
+};
+itemsCart.appendChild(clear);
 
-function createItem(barcod, name, price, quantity, amount = 0) {
+fillItems();
+
+async function getItems() {
+  const res = await fetch("https://fakestoreapi.com/products");
+  const data = await res.json();
+  data.forEach((item) => (item.quantity = 10));
+  console.log(data);
+  itemsSet = [
+    await data.forEach((item) =>
+      items.push(createItem(item.id, item.title, item.price, item.quantity))
+    ),
+  ];
+  createItemList(items);
+}
+
+function fillItems() {
+  items = [];
+  if (store && store[2]) {
+    items = JSON.parse(localStorage.getItem("store"));
+    console.log(items);
+    createItemList(items);
+    rerender();
+  } else {
+    getItems();
+  }
+}
+
+function createItem(id, title, price, quantity, amount = 0) {
   return {
-    barcod: barcod,
-    name: name,
+    id: id,
+    title: title,
     price: price,
     amount: amount,
     quantity,
   };
 }
 
-let items = [
-  createItem(0, "milk", 8.5, 10),
-  createItem(1, "chocolate", 1.5, 10),
-  createItem(2, "bamba", 10, 10),
-  createItem(3, "bisli", 5, 10),
-];
-
 function createItemList(arr) {
   itemsUl.innerHTML = "";
   arr.forEach((item) => {
     let itemRow = document.createElement("li");
-    itemRow.innerText = `${item.name} | ${item.price}$ | ${item.quantity} Items left`;
+    itemRow.innerText = `${item.title} | ${item.price}$ | ${item.quantity} Items left`;
     itemRow.id = `${item.barcod}`;
     if (item.quantity <= 0) {
       itemRow.className = "grey";
@@ -33,9 +64,7 @@ function createItemList(arr) {
       if (item.quantity > 0) {
         item.amount++;
         item.quantity--;
-        createCartList(items);
-        createPriceAmount(items);
-        createItemList(items);
+        rerender();
       }
     };
     itemsUl.appendChild(itemRow);
@@ -47,16 +76,14 @@ function createCartList(arr) {
   arr.forEach((item) => {
     if (item.amount > 0) {
       let cartRow = document.createElement("li");
-      cartRow.innerText = `${item.name} | ${item.amount}`;
+      cartRow.innerText = `${item.title} | ${item.amount}`;
       cartRow.id = `${item.barcod}`;
       cartRow.onclick = () => {
         if (item.amount > 0) {
           item.amount--;
           item.quantity++;
         }
-        createCartList(items);
-        createPriceAmount(items);
-        createItemList(items);
+        rerender();
       };
       cartUl.appendChild(cartRow);
     }
@@ -64,29 +91,43 @@ function createCartList(arr) {
 }
 
 function createPriceAmount(arr) {
+  let cartItems = 0;
+  let sum = 0;
   price.innerHTML = "";
   breakdown.innerHTML = "";
   let receipt = "";
   arr.forEach((item) => {
     console.log(item);
     if (item.amount > 0) {
+      cartItems++;
       receipt += `<div>
-      ${item.name}   ${item.amount}           $${item.price} 
+      ${item.title}   | ${item.amount} |           $${item.price} 
         </div>`;
     }
   });
-  let sum = 0;
+
   arr.forEach((item) => {
     if (item.amount > 0) {
       sum += item.amount * item.price;
     }
   });
+
+  if (cartItems > 4 || Boolean(arr.filter((item) => item.amount > 4)[0])) {
+    console.log("you got a discount");
+    sum = sum * 0.9;
+  }
+
   let receiptRow = document.createElement("div");
   receiptRow.innerHTML = `${receipt}`;
-  price.appendChild(receiptRow);
+  breakdown.appendChild(receiptRow);
   let priceRow = document.createElement("div");
   priceRow.innerHTML = `Sum: $${sum}`;
   price.appendChild(priceRow);
 }
 
-createItemList(items);
+function rerender() {
+  createCartList(items);
+  createPriceAmount(items);
+  createItemList(items);
+  localStorage.setItem("store", JSON.stringify(items));
+}
